3a2388ec34e31dcf78e49e069464f8b8acb9aadb
alexesprit <alex.esprit@gmail.com>
2017-02-16 23:20:05 +0300

    Extend feed map

--- Crystallum/Util/KristallParams.cs
+++ Crystallum/Util/KristallParams.cs
@@ -5,8 +5,12 @@ namespace Crystallum.Util {
         public const int DISCRETENESS = 10;
 
         public static Dictionary<int, int> FEED_MAP = new Dictionary<int, int>() {
+            { 6, 2200 },
             { 8, 2200 },
-            { 10, 2000 }
+            { 10, 2000 },
+            { 12, 1500 },
+            { 14, 1400 },
+            { 30, 675 }
         };
     }
 }
